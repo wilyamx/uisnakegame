aeba2ed79703ce2c2ad008d86075f71df284b7f9
William Reña <wilyamx@gmail.com>
2024-07-23 23:30:34 +0800

    Settings view

--- UISnakeGame/Source/Settings/SNKSettingsViewController.swift
+++ UISnakeGame/Source/Settings/SNKSettingsViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import Eureka
 
-class SNKSettingsViewController: SNKViewController {
+class SNKSettingsViewController: FormViewController {
     weak var coordinator: SNKHomeCoordinator?
 
     // MARK: - View Lifecycle
@@ -18,6 +19,35 @@ class SNKSettingsViewController: SNKViewController {
         title = "Settings"
         navigationController?.isNavigationBarHidden = false
         
+        form +++ Section("Gameplay")
+        <<< TextRow() { row in
+            row.title = "User"
+            row.placeholder = "Enter name to use"
+        }
+        <<< StepperRow() {
+            $0.title = "Default Game Level"
+            $0.value = 1
+        }.cellSetup({ cell, row in
+            cell.stepper.minimumValue = 1
+            cell.stepper.maximumValue = 10
+        }).cellUpdate({ cell, row in
+            if let value = row.value {
+                row.cell.valueLabel.text = "\(Int(value))"
+            }
+        })
+        +++ Section("Sounds")
+        <<< SwitchRow() { row in
+            row.title = "Background"
+            row.value = true
+        }
+        <<< SwitchRow() { row in
+            row.title = "Character"
+            row.value = true
+        }
+        <<< SwitchRow() { row in
+            row.title = "Popups"
+        }
+
         wsrLogger.info(message: "viewDidLoad")
     }
 
